1e5a0b76a83ce3a93a5e64b0c17602ab9a411d2f
Tonya-Ostapiuk <tonya.shevchik@gmail.com>
2023-01-16 20:49:16 +0300

    task 07-01 finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,7 +3,9 @@ import { galleryItems } from './gallery-items.js';
 
 const listGallery = document.querySelector('.gallery');
 const imageMarkup = createGallery (galleryItems);
+
 listGallery.insertAdjacentHTML('beforeend', imageMarkup);
+listGallery.addEventListener('click', onGalleryContainerClick);
 
 function createGallery (galleryItems){
   return galleryItems.map(({preview, original, description}) => {
@@ -21,5 +23,26 @@ function createGallery (galleryItems){
   }).join('');
 };
 
+function onGalleryContainerClick (evt){
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+  return;
+  };
+
+  const urlOriginal = evt.target.getAttribute('data-source');
+  const instance = basicLightbox.create(`
+  <img src="${urlOriginal}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  listGallery.addEventListener('keydown', evt => {
+		if (evt.key === 'Escape') {
+			instance.close()
+		}
+	});
+  
+};
+ 
 
 console.log(galleryItems);
