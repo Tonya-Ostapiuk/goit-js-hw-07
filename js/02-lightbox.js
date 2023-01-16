cc92ab50446f68a6772ee4e300a54c5417f07f7e
Tonya-Ostapiuk <tonya.shevchik@gmail.com>
2023-01-16 21:27:28 +0300

    task 07-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const listGallery = document.querySelector('.gallery');
+const imageMarkup = createGallery (galleryItems);
+
+listGallery.insertAdjacentHTML('beforeend', imageMarkup);
+// listGallery.addEventListener('click', onGalleryContainerClick);
+
+function createGallery (galleryItems){
+  return galleryItems.map(({preview, original, description}) => {
+    return `
+    <div class="gallery__item">
+    <a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        title="${description}"
+      />
+    </a>
+  </div>`;
+  }).join('');
+};
+
+const gallerySlide = new SimpleLightbox('.gallery a', {
+	captionDelay: 250
+});
 
 console.log(galleryItems);
